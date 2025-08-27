0db0c6ffc696c1a46461e4d745b59960411f593c
ALANIS S. Mayela Alejandra     [PROV] <malanisa@ternium.com.mx>
2025-08-26 23:36:05 -0600

    Se agrega proyecto agencia de negocios

---
+++ docs/agencia_negocios/js/main.js
@@ -0,0 +1,48 @@
+/* Navbar */ 
+
+
+const menu = document.querySelector(".menu");
+const navbar = document.querySelector(".navbar");
+
+menu.addEventListener("click", () => {
+  navbar.classList.toggle("change");
+  menu.classList.toggle("change");
+});
+/* Cierra Navbar */
+
+/* Video */
+
+const video = document.querySelector(".video");
+const btn = document.querySelector(".buttons #button i");
+const bar = document.querySelector(".video-bar");
+
+const playPause = () => {
+  if (video.paused){
+    video.play()
+    btn.className = "fa fa-pause-circle";
+    video.style.opacity = "0.7";
+  }else {
+    video.pause();
+    btn.className = "fa fa-play-circle";
+    video.style.opacity = "0.3";
+  }
+};
+
+btn.addEventListener("click", () =>  {
+    playPause();
+
+});
+
+video.addEventListener("timeupdate", () => {
+  console.log(video.currentTime, video.duration);
+    const barWidth = video.currentTime / video.duration;
+    bar.style.width = `${barWidth * 100}%`;
+
+    if(video.ended){
+      btn.className = "far fa-play-circle"
+      video.style.opacity = "0.3";
+    }
+});
+
+/* Video cierre */
+
